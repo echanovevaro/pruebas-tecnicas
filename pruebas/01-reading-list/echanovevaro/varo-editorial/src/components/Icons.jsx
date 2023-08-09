c75cda07f466b8975f40ca79547dc8e3d090f188
echanovevaro <30557018+echanovevaro@users.noreply.github.com>
2023-08-09 21:41:49 +0200

    mvp

---
+++ pruebas/01-reading-list/echanovevaro/varo-editorial/src/components/Icons.jsx
@@ -0,0 +1,34 @@
+export function AddToListReaded() {
+	return (
+		<svg
+			width='100px'
+			height='100px'
+			viewBox='0 0 64 64'
+			xmlns='http://www.w3.org/2000/svg'
+			fill='black'
+			stroke='#ffff'
+			stroke-width='3'>
+			<circle cx='32' cy='32' r='24' />
+			<polyline points='44 24 28 40 20 32' />
+		</svg>
+	)
+}
+export function AddToListToRead() {
+	return (
+		<svg
+			width='100px'
+			height='100px'
+			viewBox='0 0 64 64'
+			xmlns='http://www.w3.org/2000/svg'
+			fill='black'
+			stroke='#ffff'
+			stroke-width='3'>
+			<line x1='20' y1='16' x2='56' y2='16' />
+			<line x1='20' y1='32' x2='56' y2='32' />
+			<line x1='20' y1='48' x2='56' y2='48' />
+			<line x1='8' y1='16' x2='12' y2='16' />
+			<line x1='8' y1='32' x2='12' y2='32' />
+			<line x1='8' y1='48' x2='12' y2='48' />
+		</svg>
+	)
+}
